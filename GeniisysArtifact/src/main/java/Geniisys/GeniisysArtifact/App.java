5546929c6ec6bf78ab82e43e6e51f701d863c8b0
androi <andrewgcrobes@gmail.com>
2015-03-31 20:36:00 +0800

    Initial Commit

---
+++ GeniisysArtifact/src/main/java/Geniisys/GeniisysArtifact/App.java
@@ -0,0 +1,13 @@
+package Geniisys.GeniisysArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
